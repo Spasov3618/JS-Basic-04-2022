f5ad7d19c375ab021f2342e3741d78596dd88f64
Spasov3618 <svetlio_spasov@abv.bg>
2022-04-11 12:24:02 +0300

    add

---
+++ For Loop - Lab/08. Numbers, Divisible by 9.js
@@ -0,0 +1,14 @@
+function sum(input){
+    let start = Number(input[0]);
+    let end = Number(input[1]);
+    let sum = 0;
+    let num = '';
+    for (let i = start; i< end; i++){
+        if (i%9 ===0){
+            sum+= i;
+            num  +=i +  "\n" ;
+        }
+    }
+    console.log(`The sum: ${sum}`);
+   console.log(num);
+}
