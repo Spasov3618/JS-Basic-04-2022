7ccf40e96f90f3def15727ad3d401ea453feb998
Spasov3618 <svetlio_spasov@abv.bg>
2022-04-14 10:14:36 +0300

    add

--- While Loop - Exercise/02. Exam Preparation.js
+++ While Loop - Exercise/02. Exam Preparation.js
@@ -1,3 +1,31 @@
 function exam(input) {
-    
+    let index = 0;
+    let numBadGrade = Number(input[index]);
+    index++;
+    let problem;
+    let grade =0;
+    let counter =0;
+    let badGrade = 0;
+    let lastProblem = '';
+    while((problem= input[index]) !== 'Enough'){
+index++;
+let newGrade = Number(input[index]);
+index++;
+counter++;
+grade += newGrade
+lastProblem=problem;
+if (newGrade <=4) {
+    badGrade++;
+    if (badGrade === numBadGrade) {
+        console.log(`You need a break, ${badGrade} poor grades.`);
+        return;
+    }
 }
+
+    }
+    let averageGrade = grade/counter;
+    console.log(`Average score: ${averageGrade.toFixed(2)}`);
+    console.log(`Number of problems: ${counter}`);
+    console.log(`Last problem: ${lastProblem}`);
+}
+
